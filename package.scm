8ce7149cab9d1ac2ac6a87887a023e4844657054
Justin Ethier <justin.ethier@gmail.com>
2020-02-24 13:37:29 -0500

    Setup initial package

---
+++ package.scm
@@ -0,0 +1,15 @@
+(package
+ (name           srfi-26)
+ (version        0.1)
+ (license        "MIT")
+ (authors        "Sebastian Egner")
+ (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
+ (description    "Notation for Specializing Parameters without Currying")
+ (tags           "data-structures")
+ (docs           "https://github.com/cyclone-scheme/cyclone-winds/wiki/srfi-26")
+ (test           "tests.scm")
+
+ (library
+     (name (srfi 26))
+   (description "SRFI 26 library")))
+
